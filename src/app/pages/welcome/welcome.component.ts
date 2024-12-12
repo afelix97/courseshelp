d35f1b4eace9bb5834f37d23ff4ae8faa93f1cd8
Aldo Alejandro Sanchez Felix <afore.asanchezf@coppel.com>
2024-12-11 17:46:53 -0700

    update pr

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   imports: [],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss',
+  styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
 
